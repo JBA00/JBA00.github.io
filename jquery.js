5037846d51d967ae95aa7c855dca34095f4d47b0
JBA00 <33164475+JBA00@users.noreply.github.com>
2017-12-10 16:30:00 +0100

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+     $("section-2").on('click', function(e) {
+          $("section-3").on('click', function(e) {
+               $("section-4").on('click', function(e) {
+                    $("section-5").on('click', function(e) {
     var scrollLink = $('.scroll');
     scrolllink.click(function(e) {
         e.preventDefault();
